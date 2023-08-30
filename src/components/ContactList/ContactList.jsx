1456786cd76c78122bb326274de0706020cd0b0f
Ihor Tonkovyd <142664318+ITonkovyd@users.noreply.github.com>
2023-08-30 15:24:24 +0300

    changing propTypes in ContactList and changing filtering contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,25 +4,32 @@ import { Component } from 'react';
 import { Ul } from './ContactList.styled';
 
 export default class ContactList extends Component {
-    
-
-
   render() {
-    const { contacts, filteredContacts, handleDelete } = this.props
-    const contactList = filteredContacts.length > 0 ? filteredContacts : contacts
+    const { filteredContacts, handleDelete } = this.props;
+
     return (
       <Ul>
-        {contactList.map((contact) => (
-          <ContactItem key={contact.id} id={contact.id} name={contact.name} number={contact.number} handleDelete={handleDelete} />
-        ))
-        }
+        {filteredContacts.map(contact => (
+          <ContactItem
+            key={contact.id}
+            id={contact.id}
+            name={contact.name}
+            number={contact.number}
+            handleDelete={handleDelete}
+          />
+        ))}
       </Ul>
-    )
+    );
   }
 }
 
 ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  filteredContacts: PropTypes.array.isRequired,
+  filteredContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   handleDelete: PropTypes.func.isRequired,
-}
+};
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,25 @@
-
-import { PropTypes } from 'prop-types'
-import { Component } from 'react'
-import { Input, P } from './Filter.styled'
+import { PropTypes } from 'prop-types';
+import { Component } from 'react';
+import { Input, P } from './Filter.styled';
 export default class Filter extends Component {
   render() {
+    const { filter, handleSearch } = this.props;
     return (
       <>
         <P>Find contact by name</P>
         <label htmlFor="filter">
-        <Input type="text" name="filter" onChange={this.props.handleSearch} />
-      </label>
+          <Input
+            type="text"
+            name="filter"
+            value={filter}
+            onChange={handleSearch}
+          />
+        </label>
       </>
-    )
+    );
   }
 }
 
-
 Filter.propTypes = {
-  onChange: PropTypes.func.isRequired,
-}
+  handleSearch: PropTypes.func.isRequired,
+};
